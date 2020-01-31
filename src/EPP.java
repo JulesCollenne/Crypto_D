eb2cec21f3a5126d0774df1645438979b6d8f1a0
c16009858 <jules.collenne@gmail.com>
2020-01-31 09:18:59 +0100

    D.3 fini

--- src/EPP.java
+++ src/EPP.java
@@ -1,11 +1,13 @@
 // -*- coding: utf-8 -*-
 
 import java.math.BigInteger;
+import java.util.Random;
 
 public class EPP
 {
     public static void main(String[] args)
     {
+        long t1, t2;
         BigInteger n = new BigInteger("170141183460469231731687303715884105727", 10);
 
         System.out.print("Le nombre " + n);
@@ -13,19 +15,53 @@ public class EPP
             System.out.println(" est très probablement premier!");
         else
             System.out.println(" n'est absolument pas premier!");
+
+        int sommeTentatives = 0;
+        double sommeTemps = 0;
+        BigInteger[] results;
+        int nbIter = 10;
+
+        for(int i=0; i<nbIter; i++) {
+            t1 = System.nanoTime() / 1000000;
+            results = getPrimeNumber();
+            BigInteger prime = results[0];
+            t2 = System.nanoTime() / 1000000;
+            sommeTentatives += results[1].intValue();
+            sommeTemps += (double)(t2 - t1);
+        }
+        System.out.println("Nombre de tentative moyenne : " + sommeTentatives/nbIter);
+        System.out.println("Temps moyens : " + sommeTemps/nbIter + "ms");
     }
 
     static boolean est_probablement_premier(BigInteger n)
     {
         /*
-          Modifiez cette fonction afin qu'elle retourne si oui
-          ou non l'entier n est un nombre premier, avec un taux
-          d'erreur inférieur à 1/1000 000 000 000 000 000.
+          50 = 15 * (ln(10)/ln(2))
         */
-        return n.isProbablePrime(60);
+        return n.isProbablePrime(50);
     }
-}
 
+    static BigInteger[] getPrimeNumber(){
+        Random rand = new Random();
+        BigInteger[] prime = new BigInteger[2];
+        prime[0] = new BigInteger(512,  rand);
+        int nbTentatives = 1;
+        while(!est_probablement_premier(prime[0])) {
+            prime[0] = new BigInteger(512,  rand);
+            nbTentatives++;
+        }
+        System.out.println("Nombre trouvé : " + prime[0]);
+        System.out.println("Nombre de tentatives : " + nbTentatives);
+        prime[1] = new BigInteger(Integer.toString(nbTentatives));
+        return prime;
+    }
+
+}
+/*
+    Pour 10 executions :
+    Nombre de tentative moyenne : 291
+    Temps moyens : 25.3ms
+ */
 /*
   $ make
   javac *.java
---
+++ src/RSA_raw.java
@@ -0,0 +1,57 @@
+// -*- coding: utf-8 -*-
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class RSA_raw {
+    private static BigInteger code, codeChiffré, codeDéchiffré ;
+    private static BigInteger n ;      // Le module de la clef publique
+    private static BigInteger e ;      // L'exposant de la clef publique
+    private static BigInteger d ;      // L'exposant de la clef privée
+
+    static void fabrique() {           // Fabrique d'une paire de clefs RSA (A MODIFIER)
+        Random rand = new Random();
+        BigInteger p, q, w;
+        n = new BigInteger("196520034100071057065009920573", 10);
+        e = new BigInteger("7", 10);
+        d = new BigInteger("56148581171448620129544540223", 10);
+
+        do {
+            p = new BigInteger(1024, 50, rand);
+            q = new BigInteger(1024, 50, rand);
+        } while(p.compareTo(q) == 0);
+        n = p.multiply(q);
+        w = (p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)));
+        System.out.println("n = " + n);
+        System.out.println("w = " + w);
+
+        do{
+            d = new BigInteger(w.bitLength(), rand).add(BigInteger.ONE);
+        } while(d.compareTo(w.subtract(BigInteger.ONE)) > 0 && !d.gcd(w).equals(BigInteger.ONE));
+
+        e = d.modInverse(w);
+
+    }
+
+    public static void main(String[] args) {
+        code = new BigInteger("4b594f544f", 16);
+
+        /* Affichage du code clair */
+        System.out.println("Code clair        : " + code);
+
+        fabrique();
+
+        /* Affichage des clefs utilisées */
+        System.out.println("Clef publique (n) : " + n);
+        System.out.println("Clef publique (e) : " + e);
+        System.out.println("Clef privée (d)   : " + d);
+
+        /* On effectue d'abord le chiffrement RSA du code clair avec la clef publique */
+        codeChiffré = code.modPow(e, n);
+        System.out.println("Code chiffré      : " + codeChiffré);
+
+        /* On déchiffre ensuite avec la clef privée */
+        codeDéchiffré = codeChiffré.modPow(d, n);
+        System.out.println("Code déchiffré    : " + codeDéchiffré);
+    }
+}
